98ac996243681d0e923c95b92ac0ed6db71c2731
Jack Yeung <jyeung.se@gmail.com>
2018-06-18 08:13:08 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -7,3 +7,15 @@
 #     end
 #     true
 # end
+
+def prime?(integer)
+    if integer < 2
+      false
+    elsif
+    (2..integer - 1).each do |x|
+      if (integer % x) == 0
+        return false
+        end
+    end
+    else true
+end
