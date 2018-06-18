28488328b465f48de7101c9d7c7361fd1c681ad6
Jack Yeung <jyeung.se@gmail.com>
2018-06-18 07:53:03 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,14 @@
 
-def prime?(num)
-|i| num % i != 0 
-    puts true
-  else
-    puts false
-  end
+def is_prime?(*nums)
+    nums.each do |num|
+    i = 2
+      while i < num
+        if num % i == 0
+          puts "#{num} is not a prime"
+        else
+          puts "#{num} is a prime"
+        end
+        i += 1
+      end
+   end
 end
